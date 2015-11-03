cf829d4f076aa96b58fcccd4f697381503c1b6e2
lyq <lyq@debian-lyq>
2015-11-03 17:29:47 +0800

    init

--- boot/arch/armv7a-le/zynq7000/bootmain.c
+++ boot/arch/armv7a-le/zynq7000/bootmain.c
@@ -15,10 +15,21 @@
 void (*uart_init)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x4);
 void (*uart_enable)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x8);
 void (*uart_spin_puts)(const char *) = (void *)(PRELOAD_VECTOR_BASE + 0xC);
+void (*sd_dma_spin_write)(u32, u16, u32) = (void*)(PRELOAD_VECTOR_BASE + 0x10);
 
 void mbr_bootmain(void)
 {
+	int ret;
+	volatile u8 *my_kernel = (void *)0x200000;
+
 	uart_spin_puts("Hello, AIMv6!\r\n");
+	ret = sd_dma_spin_read((u32)my_kernel, 1, 0);
+	if (ret == 0) {
+		uart_spin_puts("FW: MY_KERNEL read OK.\r\n");
+	} else {
+		uart_spin_puts("FW: MY_KERNEL read failed.\r\n");
+		while(1);
+	}
 	while(1);
 }
 
